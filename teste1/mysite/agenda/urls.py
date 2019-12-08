bb41b8aafa0ac81a996263d8ec9f5675683e36ee
GustavoGarciaPereira <gusgurtavo@gmail.com>
2019-12-08 18:58:15 -0300

    delete e update

--- teste1/mysite/agenda/urls.py
+++ teste1/mysite/agenda/urls.py
@@ -16,14 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
-from .views import AlunoCreate,AlunoList,ProfessorCreate,ProfessorList,TurmaCreate,TurmaList,Home
+from .views import AlunoCreate,AlunoList,ProfessorCreate,ProfessorList,TurmaCreate,TurmaList,ProfessorUpdate,Home,ProfessorDelete
 urlpatterns = [
     path('cadastrar_aluno', AlunoCreate.as_view(), name='cadastrar_Aluno'),
     path('listar_aluno', AlunoList.as_view(), name='listar_Aluno'),
+    path('alterar_aluno/<int:pk>/', ProfessorUpdate.as_view(), name='alterar_aluno'),
+    path('deletar_aluno/<int:pk>/', ProfessorDelete.as_view(), name='deletar_aluno'),
+
     path('cadastrar_professor', ProfessorCreate.as_view(), name='cadastrar_professor'),
-    path('listar_professor', ProfessorList.as_view(), name='listar_Professor'),
+    path('listar_professor', ProfessorList.as_view(), name='listar_professor'),
+    path('alterar_professor/<int:pk>/', ProfessorUpdate.as_view(), name='alterar_professor'),
+    path('deletar_professor/<int:pk>/', ProfessorDelete.as_view(), name='deletar_professor'),
+
     path('cadastrar_turma', TurmaCreate.as_view(), name='cadastrar_turma'),
     path('listar_turma', TurmaList.as_view(), name='listar_turma'),
+    path('alterar_turma/<int:pk>/', ProfessorUpdate.as_view(), name='alterar_turma'),
+    path('deletar_turma/<int:pk>/', ProfessorDelete.as_view(), name='deletar_turma'),
     path('', Home.as_view(), name='home'),
     
     
--- teste1/mysite/agenda/views.py
+++ teste1/mysite/agenda/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.views.generic import CreateView,ListView
+from django.views.generic import CreateView,ListView,UpdateView,DeleteView,TemplateView
+
+from django.contrib import messages
+
 from .models import Aluno,Professor, Turma
+from django.urls import reverse,reverse_lazy
 
-
-from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = "home/home.html"
+
+
 
 class TurmaCreate(CreateView):
     model = Turma
@@ -22,6 +26,23 @@ class TurmaList(ListView):
     model = Turma
     template_name = "turma/turma_list.html"
 
+
+class TurmaUpdate(UpdateView):
+    model = Turma
+    template_name = 'turma/turma_form.html'
+    fields = ['professor','aluno','disciplina','plano_ensino']
+
+    success_url = "listar_turma"
+
+
+class TurmaDelete(DeleteView):
+    model = Professor
+    template_name = 'truma/confirm_delete.html'
+    success_url = reverse_lazy('listar_professor')
+
+##----------------------------------------------------
+
+
 class AlunoCreate(CreateView):
     model = Aluno
     template_name = 'aluno/aluno_form.html'
@@ -33,6 +54,21 @@ class AlunoList(ListView):
     model = Aluno
     template_name = "aluno/aluno_list.html"
 
+class AlunoUpdate(UpdateView):
+    model = Aluno
+    template_name = 'aluno/aluno_form.html'
+    fields = ['nome','matricula','nota']
+
+    success_url = "listar_aluno"
+
+class AlunoDelete(DeleteView):
+    model = Professor
+    template_name = 'aluno/confirm_delete.html'
+    success_url = reverse_lazy('listar_professor')
+
+
+#--------------------------------------------------------
+
 class ProfessorCreate(CreateView):
     model = Professor
     template_name = 'professor/professor_form.html'
@@ -43,3 +79,16 @@ class ProfessorCreate(CreateView):
 class ProfessorList(ListView):
     model = Professor
     template_name = "professor/professor_list.html"
+
+class ProfessorUpdate(UpdateView):
+    model = Professor
+    fields = ['nome','email']
+    template_name = 'professor/professor_form.html'
+    def get_success_url(self):
+        return reverse('listar_professor')
+
+class ProfessorDelete(DeleteView):
+    model = Professor
+    template_name = 'professor/confirm_delete.html'
+    success_url = reverse_lazy('listar_professor')
+    
